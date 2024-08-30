cbe4910df45164253e84643f46b046dab7441452
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-08-30 09:57:59 +0000

    データセット選択メニュー

--- lesson.py
+++ lesson.py
@@ -156,3 +156,138 @@ for i in range(3):
 for i in range(3):
     print(*a[i])
 #########################################
+#要素数 N の数列 A と数値 M が与えられます。A の M 番目の値を出力してください。
+n,m = map(int,input().split())
+a = list(map(int,input().split()))
+print(a[m-1])
+#解答コード
+N, M = map(int, input().split())
+A = [int(x) for x in input().split()]
+
+print(A[M-1])
+############################################
+#縦 H マス、横 W マスの H × W マスからなる迷路 S があります。
+#上から i 行目、左から j 列目のマス は S_ij とあらわされ、 S_ij が「#」のとき壁であり、
+#「.」のとき道です。整数 r、c が与えられるので、S_rc が壁かどうか判定してください。
+#S_rc が壁なら「Yes」を、壁ではないなら「No」と出力してください。
+H,W,r,c = map(int,input().split())
+a = []
+for i in range(H):
+    row = list(input())#list()不要
+    a.append(row)
+if a[r-1][c-1] == "#":
+    print("Yes")
+else:
+    print("No")
+#解答コード
+H, W, r, c = map(int, input().split())
+maze = [input() for _ in range(H)]
+
+if maze[r-1][c-1] == "#":
+    print("Yes")
+else:
+    print("No")
+###########################################
+#0 以上 9 以下の整数が N 個与えられます。各数値の出現回数を求め、
+#「0」の出現回数、「1」の出現回数、...「9」の出現回数、をこの順に半角スペース区切りで1行に出力してください。
+n = int(input())
+numbers = map(int,input().split())
+a = [0] * 10
+for i in numbers:
+    a[i] += 1
+print(*a)
+#解答コード
+N = int(input())
+A = [int(x) for x in input().split()]
+
+count = [0] * 10
+for num in A:
+    count[num] += 1
+
+print(" ".join(map(str, count)))
+###########################################################
+#文字列が N 個与えられます。各文字列の出現回数を文字列の辞書順に出力してください。
+n = int(input())
+a = []
+for i in range(n):
+    word = input()
+    a.append(word)
+unique_a = sorted(set(a))
+for i in unique_a:
+    count = a.count(i)
+    print(i,count)
+#解答コード
+N = int(input())
+count = {}
+
+for _ in range(N):
+    s = input()
+    if s not in count:
+        count[s] = 1
+    else:
+        count[s] += 1
+
+for word, times in sorted(count.items()):
+    print(word, times)
+###########################################
+#N 個の文字列 S_1, ... , S_N と、Q 個の文字列 T_1, ... , T_Q が与えられます。各 T_i について、以下の処理を行ってください。
+#・ S_j == T_i を満たす最小の j を出力する。ただし、そのような j が存在しない場合は -1 を出力する。
+N,Q = map(int,input().split())
+s_list = []
+for i in range(N):
+    s = input()
+    s_list.append(s)
+for i in range(Q):
+    word = input()
+    if word in s_list:
+        print(s_list.index(word) + 1)
+    else:
+        print(-1)
+#解答コード
+N, Q = map(int, input().split())
+
+S = {}
+for i in range(N):
+    s = input()
+    if s not in S:
+        S[s] = i + 1
+
+for _ in range(Q):
+    t = input()
+    if t in S:
+        print(S[t])
+    else:
+        print(-1)
+###########################################
+#N 個の要素からなる数列 A が与えられます。数列 A は昇順にソートされています。A の重複した要素を取り除いて昇順に出力してください。
+N = int(input())
+A = list(map(int,input().split()))
+unique_sortA = sorted(set(A))
+print(*unique_sortA)
+#解答コード
+N = int(input())
+A = {int(x) for x in input().split()}
+
+print(" ".join(map(str, A)))
+############################################
+#N 個の要素からなる数列 A が与えられます。2 ≦ i ≦ N の各 i に対して、A_i と同じ値が A_1 から A_{i-1} の間にあるかどうかを判定してください。
+N = int(input())
+a = list(map(int,input().split()))
+index = 1
+for i in a[1:N+1]:
+    if i in a[0:index]:
+        print("Yes")
+    else:
+        print("No")
+    index += 1
+#解答コード
+N = int(input())
+A = [int(x) for x in input().split()]
+
+memo = {A[0]}
+for i in range(1, N):
+    if A[i] in memo:
+        print("Yes")
+    else:
+        memo.add(A[i])
+        print("No")
